eb01d833eb31603507157100246da775ce612f45
iNety_x <70720366+Lewysan@users.noreply.github.com>
2023-07-14 03:09:22 -0700

    Update python3.sh

--- bin/python3.sh
+++ bin/python3.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/bash
 
+function optPython3()
+{
 sudo apt-get upgrade -y
 wget https://www.python.org/ftp/python/3.6.4/Python-3.6.4.tgz
 tar xvf Python-3.6.4.tgz
@@ -7,5 +9,6 @@ cd Python-3.6.4
 ./configure --enable-optimizations
 make -j8
 sudo make altinstall
-
+}
+optPython3
 # Try it then.
