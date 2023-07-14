7e521c359c54009d881fa53a0e9a1a8284dbbf31
iNety_x <70720366+Lewysan@users.noreply.github.com>
2023-07-14 03:08:45 -0700

    Create python3.sh

---
+++ bin/python3.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+
+sudo apt-get upgrade -y
+wget https://www.python.org/ftp/python/3.6.4/Python-3.6.4.tgz
+tar xvf Python-3.6.4.tgz
+cd Python-3.6.4
+./configure --enable-optimizations
+make -j8
+sudo make altinstall
+
+# Try it then.
