71c9a5a4e8569788e1af880a614ecd0c6a8a12b4
iNety_x <70720366+Lewysan@users.noreply.github.com>
2023-07-13 22:45:51 -0700

    Create vim-keymap.sh

---
+++ bin/vim-keymap.sh
@@ -0,0 +1,10 @@
+function zle-keymap-select
+{
+  if [[ $KEYMAP == vicmd ]] || [[ $1 = 'block' ]]; then
+    echo -ne '\e[1 q'
+  elif [[ $KEYMAP == main ]] || [[ $KEYMAP == viins ]] || [[ $KEYMAP = '' ]] || [[ $1 = 'beam' ]]; then
+    echo -ne '\e[5 q'
+  fi
+}
+zle -N zle-keymap-select
+zle-line-init() { zle-keymap-select 'beam'}
