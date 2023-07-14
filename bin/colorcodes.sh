6772994809e4e8b9b5b2b67f6e072201fafc5b5a
iNety_x <70720366+Lewysan@users.noreply.github.com>
2023-07-13 22:04:34 -0700

    Create colorcodes.sh

---
+++ bin/colorcodes.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Colors
+green="\e[0;32m\033[1m"
+white='\033[0;37m'
+black='\033[0;30m'
+clear='\033[0m'
+red="\e[0;31m\033[1m"
+purple="\e[0;35m\033[1m"
+magenta='\033[0;35m'
+cyan='\033[0;36m'
+gray="\e[0;37m\033[1m"
+blue="\e[0;34m\033[1m"
+yellow="\e[0;33m\033[1m"
