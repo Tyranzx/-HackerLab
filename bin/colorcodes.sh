b71e24f98d41b0cf7bdf588118dd764da2eaf9bd
Tyranzx <70720366+Lewysan@users.noreply.github.com>
2023-10-17 01:17:15 +0300

    Update colorcodes.sh

--- bin/colorcodes.sh
+++ bin/colorcodes.sh
@@ -3,7 +3,7 @@
 # Colors
 orange="\033[0;33m"
 green="\e[0;32m\033[1m"
-white='\033[0;37m'
+gray='\033[0;37m'
 black='\033[0;30m'
 clear='\033[0m'
 lightred="\033[1;31m"
@@ -12,7 +12,7 @@ lightpurple="\033[1;35m"
 purple="\033[0;35m"
 cyan="\033[0;36m"
 lightcyan="\033[1;36m"
-gray="\e[0;37m\033[1m"
+white="\e[0;37m\033[1m"
 blued="\033[1;34m"
 blue="\e[0;34m\033[1m"
 yellow="\e[0;33m\033[1m"
